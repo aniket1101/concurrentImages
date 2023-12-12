fc0a875fe53412de28cd512306b2e8f08e8042cd
aniket1101 <1.aniketgupta@gmail.com>
2023-12-12 21:02:24 +0000

    Added ThreadPool <.h, .c> to Makefile and implemented thread_pool_init and made structs for t_pool and node

--- ThreadPool.c
+++ ThreadPool.c
@@ -1,1 +1,16 @@
 #include "ThreadPool.h"
+
+bool thread_pool_init(struct t_pool *t_pool) {
+    t_pool->head = malloc(sizeof(struct node));
+    t_pool->tail = malloc(sizeof(struct node));
+    if (t_pool->head == NULL || t_pool->tail == NULL) {
+        return false;
+    }
+
+    t_pool->head->prev = NULL;
+    t_pool->head->next = t_pool->tail;
+    t_pool->tail->prev = t_pool->head;
+    t_pool->tail->next = NULL;
+
+    return true;
+}
--- ThreadPool.h
+++ ThreadPool.h
@@ -1,3 +1,17 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
+
+struct t_pool {
+    struct node *head;
+    struct node *tail;
+};
+
+struct node {
+    struct node *prev;
+    struct node *next;
+    pthread_t thread;
+};
+
+bool thread_pool_init(struct t_pool *t_pool);
