c82e51b07c40573b7191d37b5681e80d9ef1db25
aniket1101 <1.aniketgupta@gmail.com>
2023-12-14 01:42:27 +0000

    Implemented code for simple experiement on all partitioning implementations and added frank.jpg image.

--- BlurExprmt.c
+++ BlurExprmt.c
@@ -1,15 +1,54 @@
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
+#include <time.h>
 #include "Utils.h"
 #include "Picture.h"
 #include "PicProcess.h"
 
 // ---------- MAIN PROGRAM ---------- \\
 
+#define FROM_MILLIS 1000
+
+static void blur_func(void (*partition_func) (struct picture *pic));
+
   int main(int argc, char **argv){
 
     printf("Support Code for Running the Blur Optimisation Experiments... \n");
-    //TODO: implement your blur optimisation experiments in this file
+    
+    printf("Sequential blur takes:\n");
+    blur_func(&blur_picture);
 
+    printf("Maximumly parallelised blur takes:\n");
+    blur_func(&parallel_blur_picture);
+
+    printf("Blurring by column takes:\n");
+    blur_func(&col_blur_picture);
+
+    printf("Blurring  by row takes:\n");
+    blur_func(&row_blur_picture);
+
+    printf("Blur picture by quarter took on average:\n");
+    blur_func(&quarter_blur_picture);
   }
+
+static void blur_func(void (*partition_func) (struct picture *pic)) {
+    long long avg_time = 0;
+    struct timeval start, stop;
+    struct picture pic;
+
+    /* Loops 100 times to see how long each partition implementation takes 
+       to blur. */
+    for (int i = 0; i < 100; i++) {
+        init_picture_from_file(&pic, "test_images/frank.jpg");
+        gettimeofday(&start, NULL);
+        partition_func(&pic);
+        gettimeofday(&stop, NULL);
+        avg_time += (stop.tv_sec - start.tv_sec) * FROM_MILLIS +
+                    (stop.tv_usec - start.tv_usec) / FROM_MILLIS;
+    }
+
+    // Compute average time by dividing by number of tests
+    avg_time /= 100;
+    printf("%llu milliseconds\n", avg_time);
+}
